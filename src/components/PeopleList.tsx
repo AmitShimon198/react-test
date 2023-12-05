3b160f99569196394ee4f356bb316a95f9da10ec
amitvolga <amit.shimon@gmail.com>
2023-12-05 22:00:12 +0200

    Fix: set id as key

--- src/components/PeopleList.tsx
+++ src/components/PeopleList.tsx
@@ -9,8 +9,8 @@ const PeopleList: FunctionComponent<PeopleListProps> = ({ peoples }) => {
 
     return (
         <>
-            {peoples.map(({ first_name, last_name, country }) => (
-                <div>
+            {peoples.map(({ first_name, last_name, country, id }) => (
+                <div key={id}>
                     <p>{first_name} {last_name}</p>
                     <Flag countryCode={country} />
                 </div>))
