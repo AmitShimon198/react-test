21001726829606fcf7624ca57cf85e998772197b
amitvolga <amit.shimon@gmail.com>
2023-12-10 14:22:30 +0200

    Fix: clean code

--- src/components/CountryContextProvider.tsx
+++ src/components/CountryContextProvider.tsx
@@ -1,22 +1,17 @@
-import { Country } from 'DataApi/country.interface';
-import React, { useEffect, FunctionComponent, useState } from 'react';
+import React, { useEffect, FunctionComponent, useCallback } from 'react';
 import countryService from 'services/country.service';
 import { CountryContext } from './CountryContext';
 
 const CountryContextProvider: FunctionComponent = ({ children }) => {
-    const [countriesObject, setCountriesObject] = useState<{ [key: string]: Country }>({})
     useEffect(() => {
-        const initData = async () => {
-            const { countriesObject: countriesObjectFromService } = await countryService.getAndMapCountries();
-            setCountriesObject(countriesObjectFromService)
-        }
-        initData();
+        countryService.getAndMapCountries();
     }, [])
+    const getCountryByAlpha2Code = useCallback(({ alpha2Code }) => countryService.getCountryByAlpha2Code({ search: alpha2Code }), []);
 
     return (
         <CountryContext.Provider
             value={{
-                countriesObject
+                getCountryByAlpha2Code
             }}
         >
             {children}
