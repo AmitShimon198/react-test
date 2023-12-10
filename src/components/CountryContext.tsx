43d957399cc6abc291f31dc924e87b9bf812e8dd
amitvolga <amit.shimon@gmail.com>
2023-12-10 14:16:26 +0200

    Fix: context and context type

--- src/components/CountryContext.tsx
+++ src/components/CountryContext.tsx
@@ -1,4 +1,11 @@
+import { Country } from 'DataApi/country.interface';
 import { createContext } from 'react';
-export const CountryContext = createContext({
-    countriesObject: {}
+export const CountryContext = createContext<{
+    getCountryByAlpha2Code: ({ alpha2Code }: {
+        alpha2Code: any;
+    }) => Country | undefined
+}>({
+    getCountryByAlpha2Code: ({ alpha2Code }: {
+        alpha2Code: any;
+    }) => (undefined)
 });
